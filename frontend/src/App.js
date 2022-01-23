94eb618b17df3f05dac31493850abe63e8c9b833
Gursahib Singh <g352sing@uwaterloo.ca>
2022-01-23 00:26:53 -0500

    Initializing components dir with Navbar. Awaiting further changes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar name={"Nav"}/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Navbar.css'
+
+export default function Navbar(props) {
+  return(
+        <div id="Navbar-container" >
+            Hello {props.name}
+        </div>
+    );
+}
