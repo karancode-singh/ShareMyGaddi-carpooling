997428214e2d8227430671d2dd3f1fb6c2fdaefe
KJ <karanjot21@gmail.com>
2022-01-26 19:13:49 -0500

    started coding navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.css'
+import Navbar from './components/Navbar/Navbar'
+
+import Home from './temp-pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar name={"Nav"}/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={Home} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
