fe424f2874adbdb37dd3dba7637c2793b45fddac
Gursahib Singh <g352sing@uwaterloo.ca>
2022-01-31 10:09:54 -0500

    Changing some validation text

--- backend/Routes/authentication.js
+++ backend/Routes/authentication.js
@@ -7,14 +7,14 @@ import {signout,signup,signin} from "../Controllers/authenticate.js";
 
 router.post("/signup",[
     check("name","name should be atleast 5 characters long").isLength({min:5}),
-    check("email","name should be atleast 5 characters long").isEmail(),
-    check("password","Should be atleast 3 char").isLength({min:3})
+    check("email","not a valid email").isEmail(),
+    check("password","password should be atleast 3 char").isLength({min:3})
 
 ],signup)
 
 router.post("/signin",[
-    check("email","name should be atleast 5 characters long").isEmail(),
-    check("password","Should be atleast 3 char").isLength({min:3})
+    check("email","not a valid email").isEmail(),
+    check("password","password should be atleast 3 char").isLength({min:3})
 
 ],signin)
 
