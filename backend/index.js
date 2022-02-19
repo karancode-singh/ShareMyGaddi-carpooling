897c6810d098eb22cc6a6967430565080271b32c
Gursahib Singh <g352sing@uwaterloo.ca>
2022-02-18 23:23:00 -0500

    Adding mongoDb atlas connection

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,11 @@ dotenv.config()
 const port = process.env.PORT || 5000;
 
 // MongoDb connection
-mongoose.connect(process.env.DATABASE)
-.then(console.log("DB Connected"))
-.catch(error => console.log(error));
-
-app.listen(port,()=>{
-    console.log(`Listening to port ${port}`);
+mongoose.connect(process.env.DATABASE_URI,{ useNewUrlParser: true })
+.then(() => {
+    console.log("DB Connected")
+    app.listen(port,()=>{
+        console.log(`Listening to port ${port}`);
+    })
 })
+.catch(error => console.log(error));
