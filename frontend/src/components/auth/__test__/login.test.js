421720661a82614e146b2555e61f989bfb638e59
Prikshit Batta <prikshitbatta@gmail.com>
2022-02-24 03:53:39 +0530

    feat(test cases app.js and Login.js)

--- frontend/src/components/auth/__test__/login.test.js
+++ frontend/src/components/auth/__test__/login.test.js
@@ -1,8 +1,30 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-import Login from "../Login";
-it ("renders without crashing", ()=> {
-    const div = document.createElement("div");
-    ReactDOM.render(<loginuser></loginuser>,div)
+import {render,fireEvent} from "@testing-library/react"
+import Login, {validateForm} from "../Login";
+// it ("renders without crashing", ()=> {
+//     const div = document.createElement("div");
+//     ReactDOM.render(<loginuser></loginuser>,div)
 
+// })
+describe ("login",()=>{
+    // test('Validate function should pass correct input', () => {
+    //     const text= true;
+    //     expect(text).toBe(true)
+
+    //  })
+
+    //  test('should be false', () => {
+    //     const text= false;
+    //     expect(text).toBe(false)
+
+    //  })
+
+     it("accepts set token props in login", () => {
+        const setToken  = ()=>{
+            return "token"
+        }
+        const wrapper = render(<Login setToken ={setToken } />);
+        expect(wrapper.props().setToken).toEqual(setToken );
+      });
 })
