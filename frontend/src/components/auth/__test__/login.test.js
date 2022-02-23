833154dcf45df0e5137dcdc4c6215d12e57b0e17
Prikshit Batta <prikshitbatta@gmail.com>
2022-02-23 05:52:23 +0530

    feat(Test cases)

---
+++ frontend/src/components/auth/__test__/login.test.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import { ReactDOM } from "react";
+// import Login from "../Login";
+// it ("renders without crashing", ()= {
+//     const div = document.createElement("div");
+//     ReactDOM.render()
+
+
+// })
