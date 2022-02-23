69403f17982e2ba9bf5c29f619f76c8876bc04a9
Prikshit Batta <prikshitbatta@gmail.com>
2022-02-23 19:58:25 +0530

    Feat (Login test)

--- frontend/src/components/auth/__test__/login.test.js
+++ frontend/src/components/auth/__test__/login.test.js
@@ -1,9 +1,8 @@
-// import React from "react";
-// import { ReactDOM } from "react";
-// import Login from "../Login";
-// it ("renders without crashing", ()= {
-//     const div = document.createElement("div");
-//     ReactDOM.render()
+import React from "react";
+import  ReactDOM  from "react-dom";
+import Login from "../Login";
+it ("renders without crashing", ()=> {
+    const div = document.createElement("div");
+    ReactDOM.render(<loginuser></loginuser>,div)
 
-
-// })
+})
