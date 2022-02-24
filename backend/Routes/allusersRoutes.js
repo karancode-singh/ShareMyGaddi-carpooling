f0a6fc95101c355945c4f2cda1e6a1dc89066c48
swapnilBaluja27 <swapnilbaluja1996@gmail.com>
2022-02-23 22:54:22 -0500

    adding test cases

--- backend/Routes/allusersRoutes.js
+++ backend/Routes/allusersRoutes.js
@@ -1,9 +1,10 @@
 /* temporary route to get all the user information*/ 
-import express from "express";
-import {allusersRoutes} from "../Controllers/allusersRoutes.js";
-
+//import express from "express";
+const express= require("express");
+//import {allusersRoutes} from "../Controllers/allusersRoutes.js";
+const {allusersRoutes} = require("../Controllers/allusersRoutes.js");
 var router = express.Router()
 
 router.get("/users",allusersRoutes)
-
-export default router;
+module.exports =router;
+//export default router;
