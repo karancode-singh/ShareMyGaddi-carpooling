aba0c0e902bc56744496c4ddd415f5fdbd423360
KJ <karanjot21@gmail.com>
2022-03-01 15:05:12 -0500

    SignUp Test file correction

--- frontend/src/components/auth/__test__/SignUp.test.js
+++ frontend/src/components/auth/__test__/SignUp.test.js
@@ -1,9 +1,6 @@
-import Enzyme, { mount, shallow } from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import React from "react";
-import Adapter from 'enzyme-adapter-react-16';
 import SignUp from "../SignUp";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 global.fetch = jest.fn(() =>
     Promise.resolve({
