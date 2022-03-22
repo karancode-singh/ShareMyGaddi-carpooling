38ba1a0cf29988b3c2dec43a97f34e24581fb227
Gursahib Singh <g352sing@uwaterloo.ca>
2022-03-22 13:13:47 -0400

    Skeleton ready to display active trip details

--- frontend/src/components/activetrip/ActiveTrip.js
+++ frontend/src/components/activetrip/ActiveTrip.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import './ActiveTrip.css'
+
 export default function ActiveTrip() {
   return (
     <>
-        <h1 id="pageTitle">ActiveTrip page</h1>
+        <h1 id="pageTitle">Active Trip page</h1>
+        <div id="activeTripContainer">
+            <div className="active-trip-card">
+                <h1>Summary</h1>
+                <div className='active-trip-row'>
+                    <h3>Source:</h3>
+                    <h3>Destination:</h3>
+                    <h3>Date:</h3>
+                </div>
+
+                <h1>Details</h1>
+                <div className='active-trip-row'>
+                    <h3>Driver:</h3>
+                    <h3>Rider(s):</h3>
+
+                </div>
+            </div>
+        </div>
     </>
   )
 }
