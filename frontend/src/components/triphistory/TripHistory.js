1ee83bfed730fdc7cc18eaf84d0a7a4b9b999c3d
Gursahib Singh <g352sing@uwaterloo.ca>
2022-03-26 19:36:56 -0400

    Removing unused imports

--- frontend/src/components/triphistory/TripHistory.js
+++ frontend/src/components/triphistory/TripHistory.js
@@ -1,6 +1,4 @@
 import {React, useEffect, useState } from 'react';
-import {Row, Col} from 'react-bootstrap'
-import * as GrIcons from 'react-icons/gr'
 import sourceImg from '../../start-location.svg';
 import destinationImg from '../../pin-location.svg';
 import dtImg from '../../date-and-time.svg';
