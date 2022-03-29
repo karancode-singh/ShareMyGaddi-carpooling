514e7a639b0adf64f39c3b96fbca38b80b33baf5
Prikshit Batta <prikshitbatta@gmail.com>
2022-03-30 00:37:28 +0530

    login test

--- frontend/cypress/integration/login_test.js
+++ frontend/cypress/integration/login_test.js
@@ -1,4 +1,7 @@
 it.only('login test',function()
 {
-    cy.visit ("http://localhost:3000/login")
+    cy.visit ("http://localhost:3000/login")   
+    cy.get('[data-test="login-form"]').click()
+   // cy.get('.signup-link').click()
+   cy.get('.navbar').click()
 })
