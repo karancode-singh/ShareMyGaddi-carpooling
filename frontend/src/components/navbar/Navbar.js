f31ded11e0766ae25f69c4e99f4f8b9d4b5c0df6
Gursahib Singh <g352sing@uwaterloo.ca>
2022-04-04 11:58:51 -0400

    Fixing aligment of Delete Profile button

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -128,7 +128,7 @@ export default function Navbar({ setToken, activeTrip, name }) {
                                     <span style={{ marginLeft: '1rem' }}>Logout</span>
                                 </Link>
                             </li>
-                            <Button variant="danger" data-test="delete-button" onClick={handleDeleteProfile}>Delete Profile</Button>
+                            <Button id="deleteProfileButton" variant="danger" data-test="delete-button" onClick={handleDeleteProfile}>Delete Profile</Button>
                         </ul>
                     </nav> : null}
                 {/* Sidebar end*/}
